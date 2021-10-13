e88ad8eff01f50d3c04aaa17f1cc8085d04fb389
Lyallvaldr <adi-01@live.dk>
2021-10-13 13:20:11 +0200

    GangGang2.0
    - added even more swag!

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+int i = 0;
 int is_cool(int number);
 
 int main(void) {
-    is_cool(21);
+    printf("Yo, throw me a swag number!\n");
+    scanf("%d", &i);
+    is_cool(i);
 }
 
 int is_cool(int number) {
-    if(number == 69 || number == 420) {
+    if(number == 69 || number == 420 || number == 21) {
         printf("nice");
         return 1;
     } else{
