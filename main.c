50d5fa49b7333a06539a65c5d98ee90d6734ab34
Lyallvaldr <adi-01@live.dk>
2021-10-13 12:50:29 +0200

    GangGang
    - added swag

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(void) {
+int is_cool(int number);
 
+int main(void) {
+    is_cool(21);
+}
+
+int is_cool(int number) {
+    if(number == 69 || number == 420) {
+        printf("nice");
+        return 1;
+    } else{
+        printf("poo poo");
+    }
+    return 0;
 }
