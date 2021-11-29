18da0b274092ca3984de9d305cfc2de55222bb3f
Rasmus Krogh Udengaard <53310290+ralleq@users.noreply.github.com>
2021-11-29 14:27:49 +0100

    Pseudocode and live input improvements
    Added: Pseudocode for the menu screen and the overview screen in tick_menu and tick_overview
    Added: A tick_for_state function for determining what state should be rendered to the terminal
    Added: A separate function for reading input read_character
    Added: Functionality to more effectively read arrow keys and other keys not included in the ASCII table. This uses a function map_non_ascii_char
    Removed: Epic cool number checker

--- main.c
+++ main.c
@@ -1,22 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <conio.h>
 
-int i = 0;
-int is_cool(int number);
+#define SPECIAL_CHAR_INDICATOR 224
+
+void tick_for_state(int state, int input);
+void tick_menu(int input);
+void tick_overview(int input);
+
+int read_character();
+int map_non_ascii_char(char character);
+
+enum keyname {
+        UP = 328, DOWN = 336, LEFT = 331, RIGHT = 333,
+        PAGE_UP = 329, PAGE_DOWN = 337, DELETE = 339};
+
+enum state {MENU, OVERVIEW, EDIT, SEARCH};
+enum state current_state = MENU;
+int program_is_running = 1;
 
 int main(void) {
-    printf("Yo, throw me a swag number!\n");
-    scanf("%d", &i);
-    is_cool(i);
+    int character_pressed = '\0';
+    while(program_is_running) {
+        if(kbhit()) {
+            /* Reads a character */
+            character_pressed = read_character();
+            printf("\n Key pressed = %c, with index of %i", character_pressed, character_pressed);
+            /* Sends signal to the right state */
+            tick_for_state(current_state, character_pressed);
+        }
+    }
+    return EXIT_SUCCESS;
 }
 
-int is_cool(int number) {
-    if(number == 69 || number == 420 || number == 42 || number == 21 || number == 1337) {
-        printf("nice");
-        return 1;
-    } else{
-        printf("poo poo");
+void tick_for_state(int state, int input) {
+    switch(state) {
+        case MENU:
+            tick_menu(input); break;
+        case OVERVIEW:
+            tick_menu(input); break;
+        case EDIT:
+            tick_menu(input); break;
+        case SEARCH:
+            tick_menu(input); break;
     }
-    return 0;
 }
+
+void tick_menu(int input) {
+    /*Title*/
+    /*Menu elements using arrow keys to navigate*/
+}
+void tick_overview(int input) {
+    /*TODO: Handle input*/
+    /*Get timestamp*/
+    /*Check if text is being written in search. If this is the case, append it to a char array*/
+
+    /*Get column lable names*/
+    /*Get entry data for rows taking into account what is written in the search box*/
+
+
+    /*TODO: Draw UI*/
+    /*Draw timestamp [maybe time of next iteration]*/
+    /*Draw searchbox*/
+    
+    /*Draw column lables*/
+    /*Draw rows*/
+}
+
+int read_character() {
+    int read_char =_getch();
+    if(read_char == SPECIAL_CHAR_INDICATOR) {
+        read_char = _getch();
+        read_char = map_non_ascii_char(read_char);
+    }
+    return read_char;
+}
+int map_non_ascii_char(char character) {
+    return pow(2, sizeof(char)*8) + character;
+}
+
+
