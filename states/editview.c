fee424756c0408f28a0d8c31970be7b44ea7aa13
CitronMeister <nightmarez1997@gmail.com>
2021-12-09 23:48:56 +0100

    updated editview
    - prepared editview for actually editing things with the inclusion of editingproduct test var and a new char array.
    - made some fail safe options to make sure you dont accidently go into or leave the editing state.

--- states/editview.c
+++ states/editview.c
@@ -5,6 +5,8 @@
 //#include "overview.h"
 #include <stdio.h>
 #include "../loadSave.h"
+int editingproduct = 0;
+char chartest[32];
 void initEditView(EditView *editView) {
 
 }
@@ -13,8 +15,10 @@ void updateEditView(EditView *editView, StateType *currentState, Overview *overv
     if(input == BACKSPACE) {
         /*Removes a character from the searchText*/
         if(overview->searchTextLength >= 1){
-            overview->searchTextLength--;
-            overview->searchText[overview->searchTextLength] = '\0';
+            if(editingproduct == 0){
+                overview->searchTextLength--;
+                overview->searchText[overview->searchTextLength] = '\0';
+            }            
         } else {
             *currentState = MENU;
         }
@@ -26,21 +30,36 @@ void updateEditView(EditView *editView, StateType *currentState, Overview *overv
     } else if(input == LEFT) {
         
     } else if(input == RIGHT) {
-
-    } else {
-        /*Adds a character to the searchText*/
-        overview->searchText[overview->searchTextLength] = input;
-        overview->searchText[overview->searchTextLength+1] = '\0';
-        overview->searchTextLength++;
+        editingproduct = 0;
+    } else if(input == ENTER){
+        if(overview->searchTextLength >= 1){
+            editingproduct = 1;                    
+        }
+    }else {
+        
+        if(editingproduct == 1){
+            
+        } else {
+            /*Adds a character to the searchText*/
+            overview->searchText[overview->searchTextLength] = input;
+            overview->searchText[overview->searchTextLength+1] = '\0';
+            overview->searchTextLength++;
+        }        
     }
 }
 void drawEditView(EditView *editView, Overview *overview) {
+    
     /*Draws temporary search box*/
     ConsolePlacement(20, 0);
     printf("Search: %s", overview->searchText);
+
     /*Draws the variable lables*/
     overviewUI();
     /*Draws a list of the first n products*/
     drawProducts(overview);
+    if(editingproduct == 1){
 
+        ConsolePlacement(4, 8);
+        //printf
+    }    
 }
